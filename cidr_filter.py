e45d9e8cc9152502c8f354b7453ae3240bb6d489
random2haju <antti.ojanen@gmail.com>
2020-11-21 22:38:30 +0100

    Update cidr_filter.py

--- cidr_filter.py
+++ cidr_filter.py
@@ -2,233 +2,48 @@
 
 from netaddr import *
 import pandas as pd
+import argparse
 
-# generating IP set of IP ranges
-microsoft_range = IPSet([
-    IPNetwork('13.64.0.0/11'),
-IPNetwork('13.96.0.0/13'),
-IPNetwork('13.104.0.0/14'),
-IPNetwork('20.33.0.0/16'),
-IPNetwork('20.34.0.0/15'),
-IPNetwork('20.36.0.0/14'),
-IPNetwork('20.40.0.0/13'),
-IPNetwork('20.48.0.0/12'),
-IPNetwork('20.64.0.0/10'),
-IPNetwork('20.128.0.0/16'),
-IPNetwork('20.130.0.0/16'),
-IPNetwork('20.135.0.0/16'),
-IPNetwork('20.136.0.0/16'),
-IPNetwork('20.140.0.0/15'),
-IPNetwork('20.143.0.0/16'),
-IPNetwork('20.144.0.0/14'),
-IPNetwork('20.150.0.0/15'),
-IPNetwork('20.157.0.0/16'),
-IPNetwork('20.160.0.0/12'),
-IPNetwork('20.176.0.0/14'),
-IPNetwork('20.180.0.0/14'),
-IPNetwork('20.184.0.0/13'),
-IPNetwork('20.192.0.0/10'),
-IPNetwork('23.96.0.0/13'),
-IPNetwork('40.64.0.0/10'),
-IPNetwork('42.159.0.0/16'),
-IPNetwork('51.4.0.0/15'),
-IPNetwork('51.8.0.0/16'),
-IPNetwork('51.10.0.0/15'),
-IPNetwork('51.12.0.0/15'),
-IPNetwork('51.18.0.0/16'),
-IPNetwork('51.51.0.0/16'),
-IPNetwork('51.53.0.0/16'),
-IPNetwork('51.103.0.0/16'),
-IPNetwork('51.104.0.0/15'),
-IPNetwork('51.107.0.0/16'),
-IPNetwork('51.116.0.0/16'),
-IPNetwork('51.120.0.0/16'),
-IPNetwork('51.124.0.0/16'),
-IPNetwork('51.132.0.0/16'),
-IPNetwork('51.136.0.0/15'),
-IPNetwork('51.138.0.0/16'),
-IPNetwork('51.140.0.0/14'),
-IPNetwork('51.144.0.0/15'),
-IPNetwork('52.96.0.0/12'),
-IPNetwork('52.112.0.0/14'),
-IPNetwork('52.120.0.0/14'),
-IPNetwork('52.125.0.0/16'),
-IPNetwork('52.126.0.0/15'),
-IPNetwork('52.130.0.0/15'),
-IPNetwork('52.132.0.0/14'),
-IPNetwork('52.136.0.0/13'),
-IPNetwork('52.145.0.0/16'),
-IPNetwork('52.146.0.0/15'),
-IPNetwork('52.148.0.0/14'),
-IPNetwork('52.152.0.0/13'),
-IPNetwork('52.160.0.0/11'),
-IPNetwork('52.224.0.0/11'),
-IPNetwork('64.4.0.0/18'),
-IPNetwork('65.52.0.0/14'),
-IPNetwork('66.119.144.0/20'),
-IPNetwork('68.18.0.0/15'),
-IPNetwork('68.154.0.0/15'),
-IPNetwork('68.210.0.0/15'),
-IPNetwork('68.218.0.0/15'),
-IPNetwork('68.220.0.0/15'),
-IPNetwork('70.37.0.0/17'),
-IPNetwork('70.37.128.0/18'),
-IPNetwork('70.152.0.0/15'),
-IPNetwork('70.156.0.0/15'),
-IPNetwork('72.144.0.0/14'),
-IPNetwork('72.152.0.0/14'),
-IPNetwork('74.160.0.0/14'),
-IPNetwork('74.176.0.0/14'),
-IPNetwork('74.224.0.0/14'),
-IPNetwork('74.234.0.0/15'),
-IPNetwork('74.240.0.0/14'),
-IPNetwork('74.248.0.0/15'),
-IPNetwork('91.190.216.0/21'),
-IPNetwork('94.245.64.0/18'),
-IPNetwork('98.64.0.0/14'),
-IPNetwork('98.70.0.0/15'),
-IPNetwork('102.37.0.0/16'),
-IPNetwork('102.133.0.0/16'),
-IPNetwork('103.9.8.0/22'),
-IPNetwork('103.25.156.0/24'),
-IPNetwork('103.25.157.0/24'),
-IPNetwork('103.25.158.0/23'),
-IPNetwork('103.36.96.0/22'),
-IPNetwork('103.255.140.0/22'),
-IPNetwork('104.40.0.0/13'),
-IPNetwork('104.146.0.0/15'),
-IPNetwork('104.208.0.0/13'),
-IPNetwork('108.140.0.0/14'),
-IPNetwork('111.221.16.0/20'),
-IPNetwork('111.221.64.0/18'),
-IPNetwork('128.94.0.0/16'),
-IPNetwork('129.75.0.0/16'),
-IPNetwork('131.107.0.0/16'),
-IPNetwork('131.253.1.0/24'),
-IPNetwork('131.253.3.0/24'),
-IPNetwork('131.253.5.0/24'),
-IPNetwork('131.253.6.0/24'),
-IPNetwork('131.253.8.0/24'),
-IPNetwork('131.253.12.0/22'),
-IPNetwork('131.253.16.0/23'),
-IPNetwork('131.253.18.0/24'),
-IPNetwork('131.253.21.0/24'),
-IPNetwork('131.253.22.0/23'),
-IPNetwork('131.253.24.0/21'),
-IPNetwork('131.253.32.0/20'),
-IPNetwork('131.253.61.0/24'),
-IPNetwork('131.253.62.0/23'),
-IPNetwork('131.253.64.0/18'),
-IPNetwork('131.253.128.0/17'),
-IPNetwork('132.245.0.0/16'),
-IPNetwork('134.170.0.0/16'),
-IPNetwork('134.177.0.0/16'),
-IPNetwork('135.149.0.0/16'),
-IPNetwork('137.116.0.0/15'),
-IPNetwork('137.135.0.0/16'),
-IPNetwork('138.91.0.0/16'),
-IPNetwork('138.196.0.0/16'),
-IPNetwork('138.239.0.0/16'),
-IPNetwork('139.217.0.0/16'),
-IPNetwork('139.219.0.0/16'),
-IPNetwork('141.251.0.0/16'),
-IPNetwork('143.64.0.0/16'),
-IPNetwork('146.147.0.0/16'),
-IPNetwork('147.145.0.0/16'),
-IPNetwork('147.243.0.0/16'),
-IPNetwork('148.7.0.0/16'),
-IPNetwork('150.171.0.0/16'),
-IPNetwork('150.242.48.0/22'),
-IPNetwork('155.62.0.0/16'),
-IPNetwork('157.54.0.0/15'),
-IPNetwork('157.56.0.0/14'),
-IPNetwork('157.60.0.0/16'),
-IPNetwork('158.158.0.0/16'),
-IPNetwork('159.27.0.0/16'),
-IPNetwork('163.228.0.0/16'),
-IPNetwork('167.105.0.0/16'),
-IPNetwork('167.220.0.0/16'),
-IPNetwork('168.61.0.0/16'),
-IPNetwork('168.62.0.0/15'),
-IPNetwork('169.138.0.0/16'),
-IPNetwork('170.165.0.0/16'),
-IPNetwork('191.232.0.0/13'),
-IPNetwork('192.32.0.0/16'),
-IPNetwork('192.48.225.0/24'),
-IPNetwork('192.84.159.0/24'),
-IPNetwork('192.84.160.0/23'),
-IPNetwork('192.197.157.0/24'),
-IPNetwork('192.237.67.0/24'),
-IPNetwork('193.149.64.0/19'),
-IPNetwork('193.221.113.0/24'),
-IPNetwork('194.69.96.0/19'),
-IPNetwork('194.110.197.0/24'),
-IPNetwork('198.105.232.0/22'),
-IPNetwork('198.137.97.0/24'),
-IPNetwork('198.180.95.0/24'),
-IPNetwork('198.180.96.0/23'),
-IPNetwork('198.200.130.0/24'),
-IPNetwork('198.206.164.0/24'),
-IPNetwork('199.30.16.0/20'),
-IPNetwork('199.60.28.0/24'),
-IPNetwork('199.74.210.0/24'),
-IPNetwork('199.103.90.0/23'),
-IPNetwork('199.103.122.0/24'),
-IPNetwork('199.242.32.0/20'),
-IPNetwork('199.242.48.0/21'),
-IPNetwork('202.89.224.0/20'),
-IPNetwork('204.13.120.0/21'),
-IPNetwork('204.14.180.0/22'),
-IPNetwork('204.79.135.0/24'),
-IPNetwork('204.79.179.0/24'),
-IPNetwork('204.79.181.0/24'),
-IPNetwork('204.79.188.0/24'),
-IPNetwork('204.79.195.0/24'),
-IPNetwork('204.79.196.0/23'),
-IPNetwork('204.79.252.0/24'),
-IPNetwork('204.152.18.0/23'),
-IPNetwork('204.152.140.0/23'),
-IPNetwork('204.231.192.0/24'),
-IPNetwork('204.231.194.0/23'),
-IPNetwork('204.231.197.0/24'),
-IPNetwork('204.231.198.0/23'),
-IPNetwork('204.231.200.0/21'),
-IPNetwork('204.231.208.0/20'),
-IPNetwork('204.231.236.0/24'),
-IPNetwork('205.174.224.0/20'),
-IPNetwork('206.138.168.0/21'),
-IPNetwork('206.191.224.0/19'),
-IPNetwork('207.46.0.0/16'),
-IPNetwork('207.68.128.0/18'),
-IPNetwork('208.68.136.0/21'),
-IPNetwork('208.76.44.0/22'),
-IPNetwork('208.84.0.0/21'),
-IPNetwork('209.240.192.0/19'),
-IPNetwork('213.199.128.0/18'),
-IPNetwork('216.32.180.0/22'),
-IPNetwork('216.220.208.0/20'),
-IPNetwork('10.0.0.0/8'),
-IPNetwork('172.16.0.0/12'),
-IPNetwork('192.168.0.0/16')
-])
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="source file what you want to input", required=True)
+parser.add_argument("-o", "--output", help="name of the output file", required=True)
+parser.add_argument("-f", "--filter", help="what filter you want to use e.g microsoft_range", required=True)
+parser.add_argument("-l", "--list", help="lists available filters", action="store_true")
+args = parser.parse_args()
+
+# passing arguments to variable
+range_filter = args.filter
+source_file = args.input
+output_file = args.output
+
+# generating mainlist and loading selected filter into mainlist
+mainlist = []
+infile = open(range_filter,'r')
+for line in infile:
+    mainlist.append(line.rstrip("\n"))
+
+infile.close()
+
+# generating IPSet from the main
+ipset = IPSet(mainlist)
 
 # for loop to generate and write IP addresses into list
 d = []
-for i in microsoft_range:
+for i in ipset:
     d.append(
         {
             'remote_address': i
         }
     )
-
 # loading IP addresses and source file into dataframe
 df1 = pd.DataFrame(d).astype('str')
-df2 = pd.read_csv("<ADD SOURCE FILE HERE>").astype({'remote_address': 'str'})
+df2 = pd.read_csv(source_file).astype({'remote_address': 'str'})
 
 # merging dataframes together. Make sure you have same column name in both dataframes that you use for merging
 df_all = df1.merge(df2.drop_duplicates(), on=['remote_address'],
                    how='right', indicator=True)
 
 # writing the output in csv
-output = df_all.loc[df_all._merge == 'both']
-output.to_csv('<OUTPUT FILE>', index=False)
+print(df_all)
+output = df_all.loc[df_all._merge == 'right_only']
+output.to_csv(output_file, index=False)
